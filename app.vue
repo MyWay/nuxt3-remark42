08b822dedbe837e55c443bb8aefc6abfb4758933
MyWay <1765284+MyWay@users.noreply.github.com>
2022-06-02 11:41:09 +0200

    Initial commit.

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <Remark />
+  </div>
+</template>
---
+++ components/remark.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div id="remark42">test</div>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    script: [{src: '/scripts/remark.js', type: 'text/javascript', body: true, defer: true}]
+})
+</script>
